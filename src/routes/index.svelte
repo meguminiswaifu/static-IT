27b856a210876ca1731d5c7d1cf629850e551881
meguminiswaifu <plsdonthurtme12@gmail.com>
2022-03-02 09:55:19 +0000

    testing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,11 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<html>
+  <head>
+    <title>hi</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    Hello world
+  </body>
+</html>
